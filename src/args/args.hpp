85aeda83ef2a63ea499d2a17b33a8e03117c94d5
micuit-cuit <contact@micuit-cuit.com>
2026-03-28 09:26:36 +0100

    Refactor argument handling: update include path for str.hpp

--- src/args/args.hpp
+++ src/args/args.hpp
@@ -1,4 +1,4 @@
 #include <string>
-#include "../str/str.hpp"
+#include "../lib/str.hpp"
 int has_option(const std::string& option, int argc, char* argv[]);
 std::string get_option_value(const std::string& option, int argc, char* argv[]);
